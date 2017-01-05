a10f779dc2ee4f33d381fff6bf96dc86ffc66084
caotritran <caotritran.14@gmail.com>
2017-01-05 15:23:32 +0700

    Update Send_File_FTP.py

--- Send_File_FTP.py
+++ Send_File_FTP.py
@@ -1,6 +1,6 @@
 import pysftp
 
-with pysftp.Connection("103.221.221.144", username="admin", password="zxcasdqwe123@@@") as sftp:
+with pysftp.Connection("103.221.221.144", username="admin", password="matkhaucuatoi") as sftp:
     with sftp.cd("web/cocghe.tk/public_html"):
         a = open("/home/tritran/Desktop/python_learn/lab1.py", "rb")
         sftp.put("/home/tritran/Desktop/python_learn/lab1.py")
