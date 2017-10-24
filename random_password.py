931197f3933bf57f6a2ac5d3b2a210f361cf4289
caotritran <caotritran.14@gmail.com>
2017-10-24 16:41:38 +0700

    Add files via upload

---
+++ random_password.py
@@ -0,0 +1,23 @@
+import random
+
+
+def passgen(n):
+    x = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    pw = "".join(random.sample(x, n))
+    print(str(pw))
+
+
+def main():
+    while True:
+        try:
+            n = int(input("Nhap do dai chuoi pass can genarate: "))
+            if n <= 0 or n >= 20:
+                print("Nhap gia tri lon hon 0 va nho hon 20!")
+            else:
+                passgen(n)
+                break
+        except ValueError:
+            print("Nhap gia tri la so!")
+
+if __name__ == "__main__":
+    main()
